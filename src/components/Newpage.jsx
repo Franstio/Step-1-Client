df764b3bbc6e4cc91a3c17757b424230634fe5b3
Franstio <franstiodiego01@gmail.com>
2024-07-01 14:25:09 +0700

    Update

--- src/components/Newpage.jsx
+++ src/components/Newpage.jsx
@@ -306,7 +306,7 @@ const Home = () => {
 
     const sendDataToStep2 = async () => {
         try {
-            const response = await apiClient.post(`http://PCL-10.local:5000/UpdateDataFromStep1`, {
+            const response = await apiClient.post(`http://2-PCL.local:5000/UpdateDataFromStep1`, {
                 name: containerName,
                 line: machine.line,
                 status: 'Waiting Dispose To Step 2',
